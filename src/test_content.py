7bf4229486a82e5db6a77ebe831e8a1856a8d054
Ryan May <rmay@ucar.edu>
2022-06-29 00:47:55 -0600

    Add some more code
    (cherry picked from commit 2aef88e74be9996edab732cbb53bb8c5d9941c17)

--- src/test_content.py
+++ src/test_content.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     import os
     for i in range(10):
         print(f'{i}...hi')
+
+    with open('test_data.txt', 'rt') as infile:
+        for line in infile:
+            print(line)
